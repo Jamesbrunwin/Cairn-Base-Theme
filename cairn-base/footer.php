e2e42735b4e376e7b9f74306cbb96b18dd8d6833
James Brunwin <brunwwwin@gmail.com>
2018-06-24 09:53:07 +0100

    Original Base Files

---
+++ cairn-base/footer.php
@@ -0,0 +1,7 @@
+    <footer>
+      <p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?></p>
+      <p class="by-one">Site by <a href="http://cairnagency.co.uk">Cairn Agency</a></p>
+    </footer>
+    <?php wp_footer(); ?>
+  </body>
+</html>
